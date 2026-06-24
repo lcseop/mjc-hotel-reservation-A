9eced7b08d0e078b824d3d1f832dd165380c7ad9
이충섭 <cndtjqdlqsle@gmail.com>
2026-06-24 20:57:41 +0900

    jwt, security, swagger 추가

--- hotel/src/main/java/com/mjc/hotel/student/entity/Student.java
+++ hotel/src/main/java/com/mjc/hotel/student/entity/Student.java
@@ -1,20 +1,24 @@
 package com.mjc.hotel.student.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.mjc.hotel.util.BaseEntity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
-public class Student {
+@EntityListeners(AuditingEntityListener.class)
+public class Student extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
