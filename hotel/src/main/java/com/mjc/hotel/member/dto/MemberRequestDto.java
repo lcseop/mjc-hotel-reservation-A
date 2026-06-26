cb2908fc06524274aaaadf6f4dcc80dbcd34fd0a
leejaewon23 <sosekee1110@gmail.com>
2026-06-26 18:44:03 +0900

    feat: member dto 구현

---
+++ hotel/src/main/java/com/mjc/hotel/member/dto/MemberRequestDto.java
@@ -0,0 +1,21 @@
+package com.mjc.hotel.member.dto;
+
+import com.mjc.hotel.member.entity.MemberRole;
+import com.mjc.hotel.member.entity.MemberStatus;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Getter
+@Setter
+public class MemberRequestDto {
+    private String name;
+    private String phone;
+    private String email;
+    private MemberStatus status;
+    private MemberRole role;
+    private Boolean emailVerified;
+    private Boolean phoneVerified;
+}
