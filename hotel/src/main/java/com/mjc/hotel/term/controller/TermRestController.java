9d3884b47a7096ed1f5be4e9230e98e2d0a6a094
leejaewon23 <sosekee1110@gmail.com>
2026-06-27 17:17:12 +0900

    rename

--- hotel/src/main/java/com/mjc/hotel/term/controller/TermRestController.java
+++ hotel/src/main/java/com/mjc/hotel/term/controller/TermRestController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/term")
 @RequiredArgsConstructor
-public class TermController {
+public class TermRestController {
 
     private final TermService termService;
 
