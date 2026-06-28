e88ad36c034783f48254a076fb140642b6ffd310
my2024081062 <ish0817@naver.com>
2026-06-29 00:40:20 +0900

    리뷰 서비스 insert

--- hotel/src/main/java/com/mjc/hotel/review/service/ReviewService.java
+++ hotel/src/main/java/com/mjc/hotel/review/service/ReviewService.java
@@ -6,16 +6,32 @@ import com.mjc.hotel.member.entity.Member;
 import com.mjc.hotel.member.repository.MemberRepository;
 import com.mjc.hotel.reservations.entity.Reservation;
 import com.mjc.hotel.reservations.repository.ReservationRepository;
-import com.mjc.hotel.review.entity.Review;
-import com.mjc.hotel.review.repository.ReviewRepository;
-import com.mjc.hotel.review.request.ReviewRequest;
+import com.mjc.hotel.review.entity.*;
+import com.mjc.hotel.review.repository.*;
+import com.mjc.hotel.review.request.ReviewCategoryRequest;
+import com.mjc.hotel.review.request.ReviewCreateRequest;
+import com.mjc.hotel.review.request.ReviewTagRequest;
+import com.mjc.hotel.review.response.ReviewCategoryResponse;
+import com.mjc.hotel.review.response.ReviewResponse;
+import com.mjc.hotel.review.response.ReviewTagResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+
+    private final ReviewCategoryMasterRepository reviewCategoryMasterRepository;
+
+    private final ReviewCategoryRepository reviewCategoryRepository;
+
+    private final ReviewTagMasterRepository reviewTagMasterRepository;
+
+    private final ReviewTagRepository reviewTagRepository;
 
     private final HotelRepository hotelRepository;
 
@@ -23,7 +39,7 @@ public class ReviewService {
 
     private final ReservationRepository reservationRepository;
 
-    public Review insertReview(ReviewRequest reviewRequest) {
+    public ReviewResponse insertReview(ReviewCreateRequest reviewRequest) {
         Hotel hotel = hotelRepository.findById(reviewRequest.getHotelId()).orElseThrow();
         Member member = memberRepository.findById(reviewRequest.getMemberId()).orElseThrow();
         Reservation reservation = reservationRepository.findById(reviewRequest.getReservationId()).orElseThrow();
@@ -36,9 +52,95 @@ public class ReviewService {
                 .rating(reviewRequest.getRating())
                 .travelType(reviewRequest.getTravelType())
                 .content(reviewRequest.getContent())
+                .likeCount(0)
+                .dislikeCount(0)
                 .build();
 
-        Review result = reviewRepository.save(review);
+        Review reviewResult = reviewRepository.save(review);
+
+        List<ReviewCategory> categories = this.insertReviewCategories(reviewRequest, reviewResult);
+
+        List<ReviewTag> tags = this.insertReviewTags(reviewRequest, reviewResult);
+
+        ReviewResponse result = ReviewResponse.builder()
+                .reviewId(reviewResult.getReviewId())
+                .hotelId(reviewResult.getHotel().getSid())
+                .memberId(reviewResult.getMember().getMemberId())
+                .reservationId(reviewResult.getReservation().getSid())
+                .rating(reviewResult.getRating())
+                .travelType(reviewResult.getTravelType())
+                .content(reviewResult.getContent())
+                .likeCount(reviewResult.getLikeCount())
+                .dislikeCount(reviewResult.getDislikeCount())
+                .categories(
+                        categories.stream()
+                                .map(reviewCategory -> ReviewCategoryResponse.builder()
+                                        .reviewCategoryId(reviewCategory.getReviewCategoryId())
+                                        .reviewCategoryMasterId(reviewCategory.getReviewCategoryMaster().getReviewCategoryMasterId())
+                                        .reviewId(reviewCategory.getReview().getReviewId())
+                                        .rating(reviewCategory.getRating())
+                                        .build())
+                                .toList()
+                )
+                .tags(
+                        tags.stream()
+                                .map(reviewTag -> ReviewTagResponse.builder()
+                                        .reviewId(reviewTag.getReview().getReviewId())
+                                        .reviewTageMapId(reviewTag.getReviewTagMaster().getReviewTagMasterId())
+                                        .build()
+                                )
+                                .toList()
+                )
+                .createdAt(reviewResult.getCreatedAt())
+                .updatedAt(reviewResult.getUpdatedAt())
+                .deletedAt(reviewResult.getDeletedAt())
+                .deleted(reviewResult.getDeleted())
+                .build();
+
         return result;
+    }
+
+    private List<ReviewCategory> insertReviewCategories(ReviewCreateRequest reviewRequest, Review reviewResult) {
+        List<ReviewCategory> categories = new ArrayList<>();
+
+        if(reviewRequest.getCategories() != null && !reviewRequest.getCategories().isEmpty()){
+
+            for(ReviewCategoryRequest reviewCategoryRequest : reviewRequest.getCategories()){
+                ReviewCategoryMaster reviewCategoryMaster = reviewCategoryMasterRepository
+                        .findById(reviewCategoryRequest.getCategoryId())
+                        .orElseThrow();
+
+                ReviewCategory reviewCategory = ReviewCategory.builder()
+                        .review(reviewResult)
+                        .reviewCategoryMaster(reviewCategoryMaster)
+                        .rating(reviewCategoryRequest.getRating())
+                        .build();
+
+                ReviewCategory categoryResult = reviewCategoryRepository.save(reviewCategory);
+
+                categories.add(categoryResult);
+            }
+        }
+
+        return categories;
+    }
+
+    private List<ReviewTag> insertReviewTags(ReviewCreateRequest reviewRequest, Review reviewResult) {
+        List<ReviewTag> tags = new ArrayList<>();
+        if(reviewRequest.getTags() != null && !reviewRequest.getTags().isEmpty()) {
+            for(ReviewTagRequest reviewTagRequest : reviewRequest.getTags()) {
+                ReviewTagMaster reviewTagMaster = reviewTagMasterRepository.findById(reviewTagRequest.getTagId()).orElseThrow();
+
+                ReviewTag reviewTag = ReviewTag.builder()
+                        .review(reviewResult)
+                        .reviewTagMaster(reviewTagMaster)
+                        .build();
+
+                ReviewTag tagResult = reviewTagRepository.save(reviewTag);
+
+                tags.add(tagResult);
+            }
+        }
+        return tags;
     }
 }
