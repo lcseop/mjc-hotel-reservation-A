50f83b0dc6d123925a08935c508dea6812dda2a8
my2024081062 <ish0817@naver.com>
2026-06-29 09:42:22 +0900

    복합키 entity의 id는 키 클래스여야해서 수정함

--- hotel/src/main/java/com/mjc/hotel/review/repository/ReviewReactionRepository.java
+++ hotel/src/main/java/com/mjc/hotel/review/repository/ReviewReactionRepository.java
@@ -1,9 +1,10 @@
 package com.mjc.hotel.review.repository;
 
 import com.mjc.hotel.review.entity.ReviewReaction;
+import com.mjc.hotel.review.entity.composite_key.ReviewReactionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewReactionRepository extends JpaRepository<ReviewReaction, Long> {
+public interface ReviewReactionRepository extends JpaRepository<ReviewReaction, ReviewReactionId> {
 }
