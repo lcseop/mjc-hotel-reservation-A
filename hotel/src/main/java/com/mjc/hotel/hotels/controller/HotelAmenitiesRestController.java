3b895be604634ffe135b4215a8efe3858c6fbd92
이충섭 <cndtjqdlqsle@gmail.com>
2026-07-01 12:17:13 +0900

    roomServiceTest 추가된 속성에 맞게 변경, 편의시설 컨트롤러 추가

--- hotel/src/main/java/com/mjc/hotel/hotels/controller/HotelAmenitiesRestController.java
+++ hotel/src/main/java/com/mjc/hotel/hotels/controller/HotelAmenitiesRestController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/hotel/ame")
@@ -36,10 +33,10 @@ public class HotelAmenitiesRestController {
             summary = "호텔 편의시설 수정",
             description = "호텔의 편의시설 데이터를 수정합니다.\n편의시설의 title에는 제목이, description에는 간단한 설명이 제공됩니다."
     )
-    @PostMapping
+    @PatchMapping
     public ResponseEntity<ApiResponse<HotelAmenitiesDto>> update(@RequestBody HotelAmenitiesDto dto) {
         HotelAmenitiesDto update = hotelAmenitiesService.update(dto);
-        return ResponseEntity.status(201).body(
+        return ResponseEntity.status(200).body(
                 new ApiResponse<>(ResponseCode.SUCCESS, "hotel update success", update)
         );
     }
